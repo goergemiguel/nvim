84b074a2be628293340459addfbe95923c4ea66c
goergemiguel <goergemiguel21@gmail.com>
2023-04-10 20:04:55 +0800

    fix(harpoon): add width to harpoon plugin quick menu

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,7 +3,11 @@ return {
 	lazy = true,
 	event = "BufRead",
 	config = function()
-		require("harpoon").setup({})
+		require("harpoon").setup({
+			menu = {
+				width = 100,
+			},
+		})
 		local keymap = vim.keymap
 		keymap.set("n", "<leader>hm", ":lua require('harpoon.ui').toggle_quick_menu()<CR>") -- Harpoon Marks, show list of harpooned marks
 		keymap.set("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>") -- Harpoon Add, add to list of harpooned
