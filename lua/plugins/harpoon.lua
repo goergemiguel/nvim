001513d62c68ec55e1e240bcaf92dce16e0cac8d
goergemiguel <goergemiguel21@gmail.com>
2023-04-10 19:19:26 +0800

    feat: add harpoon plugin

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,11 @@
+return {
+	"ThePrimeagen/harpoon",
+	lazy = true,
+	event = "BufRead",
+	config = function()
+		require("harpoon").setup({})
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>hm", ":lua require('harpoon.ui').toggle_quick_menu()<CR>") -- Harpoon Marks, show list of harpooned marks
+		keymap.set("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>") -- Harpoon Add, add to list of harpooned
+	end,
+}
