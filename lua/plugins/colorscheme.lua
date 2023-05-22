c89f6930331db5d940138a4b0724277045507656
goergemiguel <goergemiguel21@gmail.com>
2023-05-22 23:13:52 +0800

    feat: update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,17 @@
 return {
 	"rebelot/kanagawa.nvim", -- preferred colorscheme
 	config = function()
-		vim.cmd("colorscheme kanagawa-wave")
+		require("kanagawa").setup({
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
+		})
+		vim.cmd("colorscheme kanagawa-dragon")
 	end,
 }
