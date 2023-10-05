bd9b553159fae2b673b1e7264cef94590a32eaa3
goergemiguel <goergemiguel21@gmail.com>
2023-10-06 00:50:44 +0800

    fix: broken indent-blankline plugin setup

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,9 +3,6 @@ return {
 	lazy = true,
 	event = "BufRead",
 	config = function()
-		require("indent_blankline").setup({
-			show_current_context = true,
-			show_current_context_start = true,
-		})
+		require("ibl").setup()
 	end,
 }
